30692ccd3b1a6aca50011b0dccce296d71819722
Paul Stan <Paul Stan>
2022-11-22 08:50:01 +0200

    Update index.html, sb-admin-2.js, and 3 more files...

---
+++ src/components/SideBar.vue
@@ -0,0 +1,84 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SidebarBrand from './SidebarBrand.vue';
+
+export default defineComponent({
+    components: {
+        SidebarBrand
+    },
+
+    methods: {
+        toggleMenu: function () {
+            this.toggled = !this.toggled;
+        }
+    },
+
+    data() {
+        return {
+            toggled: false
+        }
+    },
+})
+
+</script>
+
+<template>
+    <ul :class="{ toggled: toggled, 'navbar-nav bg-gradient-primary sidebar sidebar-dark accordion': true }">
+
+        <SidebarBrand></SidebarBrand>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Admin area
+        </div>
+
+        <!-- Nav Item - Pages Collapse Menu -->
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Service Providers</span>
+            </a>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Logged in service provider
+        </div>
+
+        <!-- Nav Item - Utilities Collapse Menu -->
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="fas fa-fw fa-wrench"></i>
+                <span>Phisical Resources</span>
+            </a>
+        </li>
+
+        <!-- Nav Item - Pages Collapse Menu -->
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="fas fa-fw fa-folder"></i>
+                <span>Reservations</span>
+            </a>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
+
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle" @click="toggleMenu"></button>
+        </div>
+
+        <!-- Sidebar Message -->
+        <div class="sidebar-card d-none d-lg-flex">
+            <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
+            <span class="text-center mb-2">Have a nice and productive day!</span>
+        </div>
+    </ul>
+</template>
--- src/components/SidebarBrand.vue
+++ src/components/SidebarBrand.vue
@@ -2,27 +2,16 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  data() {
-    return {
-      count: 1
-    }
-  },
 
-  // Lifecycle hooks are called at different stages
-  // of a component's lifecycle.
-  // This function will be called when the component is mounted.
-  mounted() {
-    console.log(`side bar component`)
-  }
 })
 
 </script>
 
 <template>
-  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
     <div class="sidebar-brand-icon rotate-n-15">
-      <i class="fas fa-laugh-wink"></i>
+      <i class="fas fa-clock"></i>
     </div>
-    <div class="sidebar-brand-text mx-3">SB Admin</div>
+    <div class="sidebar-brand-text mx-3">Timetable</div>
   </a>
 </template>
